65236afce23a1b5dfb325b5961ebe6c76b7aa1e8
Sofiane M <poubellepropre8@gmail.com>
2022-02-19 14:09:27 +0100

    update

--- player.js
+++ player.js
@@ -1,9 +1,2 @@
-class Player {
-    constructor (gameInstance) {
-      this.game = gameInstance;
-      this.x = 300;
-      this.y = 150;
-      this.width = 25;
-      this.height = 25;
-    }
-}
+
+  
--- script.js
+++ script.js
@@ -1,3 +1,37 @@
 const canvasElement = document.querySelector('canvas');
 
+class Player {
+    constructor (gameInstance) {
+    this.game = gameInstance;
+    this.x = 300;
+    this.y = 150;
+          
+        }
+    
+    draw() {
+      this.game.context.fillStyle = 'yellow';
+      this.game.context.fillRect(this.x, this.y, 50, 50);
+    }
+    }
+
+class Game {
+    constructor (canvasElement) {
+      this.canvas = canvasElement;
+      this.context = canvasElement.getContext('2d');
+      this.player = new Player(this);
+
+      this.enemies = [];
+      this.cryptos = [];
+      this.score = 0;
+    }
+
+    draw() {
+      this.player.draw();
+    }
+    }
+
 const game = new Game(canvasElement);
+
+game.draw();
+
+
